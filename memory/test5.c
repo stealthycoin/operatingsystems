236a486a4973ff5b89b58dfbcff11c5a9482f313
StealthyCoin <john.w.carlyle@gmail.com>
2014-05-15 21:15:42 -0700

    fixed makefile and a test for basic mem leak

---
+++ memory/test5.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "slug_mem.h"
+
+/* Test program to alloc memory and then exit
+ * 
+ */
+
+int main(void){
+    char *string;
+    const char* hello = "Hello, world!\n";
+    string = (char *) malloc(15*sizeof(char));
+
+    return 0;
+}
