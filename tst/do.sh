69f3beb889fbc6fadc081a791e45cc305b6b5343
Andrew <ancedwar@ucsc.edu>
2014-06-07 16:58:32 -0700

    still need to fix fsck probs

--- tst/do.sh
+++ tst/do.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 cp ../pa4/include/minix/* /usr/src/include/minix/
+cp ../pa4/commands/fsck.mfs/fsck.c /usr/src/commands/fsck.mfs
 cp ../pa4/include/* /usr/src/include/
 cp ../pa4/servers/vfs/* /usr/src/servers/vfs/
 cp ../pa4/servers/mfs/* /usr/src/servers/mfs/
