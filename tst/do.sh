091c0c2edaf85d363398172334a6d997df4d782e
Kaya <Kaya@csongor.local>
2014-06-08 14:59:53 -0700

    fixed do.sh

--- tst/do.sh
+++ tst/do.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
 cp ../pa4/include/minix/* /usr/src/include/minix/
-cp ../pa4/commands/fsck.mfs/fsck.c /usr/src/commands/fsck.mfs/
-cp ../pa4/commands/cp/cp.c /usr/src/commands/cp/
 cp ../pa4/include/* /usr/src/include/
 cp ../pa4/servers/vfs/* /usr/src/servers/vfs/
 cp ../pa4/servers/mfs/* /usr/src/servers/mfs/
